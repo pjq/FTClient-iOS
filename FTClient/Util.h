9cc939a6725046895911ad400401e7dca9734cc6
pjq <pengjianqing@gmail.com>
2012-03-29 00:03:07 +0800

    Add UI

---
+++ FTClient/Util.h
@@ -0,0 +1,14 @@
+//
+//  Util.h
+//  FTClient
+//
+//  Created by Peng Jianqing on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FTClient_Util_h
+#define FTClient_Util_h
+
+
+
+#endif
---
+++ FTClient/Util.m
@@ -0,0 +1,9 @@
+//
+//  Util.c
+//  FTClient
+//
+//  Created by Peng Jianqing on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
