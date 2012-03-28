e553f3339a017ae1e72c691b5ae751e3e3086258
Peng Jianqing <pjq@Pengs-MacBook-Pro.local>
2012-03-28 22:59:41 +0800

    Initial Commit

---
+++ FTClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FTClient
+//
+//  Created by Peng Jianqing on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FTClientTests/FTClientTests.h
@@ -0,0 +1,13 @@
+//
+//  FTClientTests.h
+//  FTClientTests
+//
+//  Created by Peng Jianqing on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FTClientTests : SenTestCase
+
+@end
